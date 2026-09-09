a93b4e62c2ae7d86565604c2e9c234e2ec34904a
deusmeajudafavorr-star <deusmeajudafavorr@gmail.com>
2026-09-09 18:03:58 -0300

    Add Google Search Console verification

--- app/layout.js
+++ app/layout.js
@@ -8,11 +8,20 @@ export const metadata = {
     shortcut: '/favicon.png',
     apple: '/favicon.png',
   },
+  verification: {
+    google: 'bhBWxdXxAmGREMu0QwknslC-seAh-0WLlqPr4CRgoX8',
+  },
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="bhBWxdXxAmGREMu0QwknslC-seAh-0WLlqPr4CRgoX8"
+        />
+      </head>
       <body>{children}</body>
     </html>
   )
